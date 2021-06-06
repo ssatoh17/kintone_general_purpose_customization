4ed377c796646c74d8edfc8f84d28c94d55a3f67
shoh38 <shoh38@outlook.jp>
2021-06-06 17:09:10 +0900

    一覧表示は非同期で処理されるため、実行開始を遅らせた

--- kintone全体のカスタマイズ/PC用JS/一覧画面で今年の日付の年部分を表示しない.js
+++ kintone全体のカスタマイズ/PC用JS/一覧画面で今年の日付の年部分を表示しない.js
@@ -1,5 +1,12 @@
-let 今年の年 = new Date().getFullYear();
-// 日付型フィールドと日時型フィールドの値を置換する
-document.querySelectorAll('td.recordlist-cell-gaia.recordlist-date-gaia, td.recordlist-cell-gaia.recordlist-datetime-gaia').forEach(elm=>{
-   elm.innerHTML = elm.innerHTML.replaceAll(`<span>${今年の年}-`,'<span>').replaceAll('-','/');
-});
+setTimeout( () =>
+{
+   let 今年の年 = new Date().getFullYear();
+   // 日付型フィールドと日時型フィールドの値を置換する
+   const 日付関連フィールドセレクタ = 'td.recordlist-cell-gaia.recordlist-date-gaia,'
+                               + 'td.recordlist-cell-gaia.recordlist-datetime-gaia,'
+                               + 'td.recordlist-cell-gaia.recordlist-created_at-gaia,'
+                               + 'td.recordlist-cell-gaia.recordlist-modified_at-gaia';
+   document.querySelectorAll(日付関連フィールドセレクタ).forEach( elm =>{
+      elm.innerHTML = elm.innerHTML.replaceAll(`<span>${今年の年}-`,'<span>').replaceAll('-','/');
+   });
+}, 3000);
